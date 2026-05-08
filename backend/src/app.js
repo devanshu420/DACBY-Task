4a8ed68eef8ac734e537b57017968c32c22bdce2
Devanshu Sharma <devanshu11122005@gmail.com>
2026-05-08 20:47:38 +0530

    Add cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,38 +1,35 @@
 const express = require("express");
-const authRoutes = require('./routes/user.routes');
-const storyRoutes = require('./routes/story.routes');
+const authRoutes = require("./routes/user.routes");
+const storyRoutes = require("./routes/story.routes");
 const scraperRoutes = require("./routes/scraper.routes");
 
 const cors = require("cors");
 
-
-
 // cookie parser
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 // create express app
 const app = express();
 
 // Cors
 
-
 app.use(
   cors({
-    origin: ["http://localhost:5173"], 
-    credentials: true, 
-  })
+    origin: [
+      "http://localhost:5173",
+      "https://web-scraper-bice-phi.vercel.app",
+    ],
+    credentials: true,
+  }),
 );
 
 // Middleware
 app.use(cookieParser());
 app.use(express.json());
 
-
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/stories', storyRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/stories", storyRoutes);
 app.use("/api/scrape", scraperRoutes);
-
-
 
 module.exports = app;
