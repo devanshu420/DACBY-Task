1751c31a7cb7177c769ea75438a9c9a4ca6d683a
Devanshu Sharma <devanshu11122005@gmail.com>
2026-05-08 21:11:45 +0530

    Update some cookie setting for deployment

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -66,8 +66,8 @@ const loginUserController = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
